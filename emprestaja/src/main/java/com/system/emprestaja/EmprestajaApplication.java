134519e97c5e28f163bffeac271fb6b43ffc8e6c
NICHOLLE GIANINNI <gianinni.nicolle@gmail.com>
2025-07-29 23:55:48 -0300

    Criado projeto spring - dependencias

---
+++ emprestaja/src/main/java/com/system/emprestaja/EmprestajaApplication.java
@@ -0,0 +1,13 @@
+package com.system.emprestaja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmprestajaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmprestajaApplication.class, args);
+	}
+
+}
---
+++ emprestaja/src/test/java/com/system/emprestaja/EmprestajaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.system.emprestaja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmprestajaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
